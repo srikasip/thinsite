84f87f412683495a2566fdde3ea0d5cc42b9c1c0
srikasip <srikasip@gmail.com>
2014-11-05 14:23:51 -0800

    Initializing Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thinsite::Application.config.secret_key_base = '99ed507c7b22f8153cdabb20687a96cb61eb4457175786b7a40b48569f035d091e39ccfc00ed1c63c41f652b4ab34fa72334a3474ab87debca0f317bcdc36f05'
